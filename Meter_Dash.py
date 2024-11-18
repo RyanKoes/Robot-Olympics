35a469592933021a8ea9bd891f9cc34d22f913a7
Andy Gao <ag042@bucknell.edu>
2024-11-18 14:49:44 -0500

    trying to add linesensor

--- Meter_Dash.py
+++ Meter_Dash.py
@@ -1,3 +1,4 @@
+import machine
 import time
 
 class bot:
@@ -24,6 +25,37 @@ class bot:
         self.M2A.duty_u16(int(speed * 65535))
         self.M2B.duty_u16(0)
 
+    def turnleft(self, amount_u16 = 0x2000):
+        # turn left by increasing the speed of the right motor and decreasing the speed of the left motor
+        # assumes we are going forward.
+        self.M1A.duty_u16(self.M1A.duty_u16())     # Duty Cycle must be between 0 until 65535
+        self.M1B.duty_u16(self.M1B.duty_u16())
+
+        if self.M2B.duty_u16() == 0:
+            # reverse
+            self.M2A.duty_u16(min(0xffff,self.M2A.duty_u16() + amount_u16))
+            self.M2B.duty_u16(0)
+        else:
+            # forward
+            self.M2A.duty_u16(self.M2A.duty_u16())
+            self.M2B.duty_u16(max(0,self.M2B.duty_u16() - amount_u16))
+
+    def turnright(self, amount_u16 = 0x2000):
+        # turn left by increasing the speed of the right motor and decreasing the speed of the left motor
+        # assumes we are going forward.
+
+        if self.M1B.duty_u16() == 0:
+            # reverse
+            self.M1A.duty_u16(min(0xffff,self.M1A.duty_u16() + amount_u16))
+            self.M1B.duty_u16(0)
+        else:
+            # forward
+            self.M1A.duty_u16(self.M1A.duty_u16())     # Duty Cycle must be between 0 until 65535
+            self.M1B.duty_u16(max(0,self.M1B.duty_u16() - amount_u16))
+
+        self.M2A.duty_u16(self.M2A.duty_u16())
+        self.M2B.duty_u16(self.M2B.duty_u16())
+
     def fwd(self, speed=0.3):
         self.M1A.duty_u16(0)     # Duty Cycle must be between 0 and 65535
         self.M1B.duty_u16(int(speed * 65535))
@@ -45,47 +77,29 @@ class bot:
     def read_line(self):
         return self.left.value(), self.right.value()
 
+conf = {
+    "M1A": 8,
+    "M1B": 9,
+    "M2A": 10,
+    "M2B": 11,
+    "left": 2,
+    "right": 3,
+}
+bot = bot(**conf)
 
-# Create a bot object
-def main():
-    conf = {
-        "M1A": 8,
-        "M1B": 9,
-        "M2A": 10,
-        "M2B": 11,
-        "left": 2,
-        "right": 3,
-    }
-    b = bot(**conf)
+while True:
+    left, right = bot.read_line()
 
-    while True:
-        left, right = b.read_line()
+    if left == 0 and right == 0:
+        bot.fwd()
+    elif left == 1 and right == 0:
+        bot.turnleft()
+    elif left == 0 and right == 1:
+        bot.turnright()
+    elif left == 1 and right == 1:
+        bot.brake()
+    else:
+        bot.fwd()
 
-        if left == 0 and right == 0:
-            # go forward
-        elif left == 1 and right == 0:
-            # turn left
-        elif left == 0 and right == 1:
-            # turn right
-        elif left == 1 and right == 1:
-            # forward
-        else:
-            # go forward
-
-        print("Left: {}, Right: {}".format(left, right))
-        time.sleep(1)
-
-try:
-    main()
-except Exception as e:
-    conf = {
-        "M1A": 8,
-        "M1B": 9,
-        "M2A": 10,
-        "M2B": 11,
-        "left": 2,
-        "right": 3,
-    }
-    b = bot(**conf)
-    print("Emergency stop.")
-    raise(e)
+    print("Left: {}, Right: {}".format(left, right))
+    time.sleep(1)
